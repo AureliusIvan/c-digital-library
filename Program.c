607ccb1e7bbcc2ecbb83389724eb3f1294cdbf36
maecyntha <maecyntha.tan@gmail.com>
2022-05-22 15:03:43 +0700

    ...

--- Program.c
+++ Program.c
@@ -355,26 +355,26 @@ char ch;
 
 
 int main(){//Main
-// FILE *dataBUKU;
-// DataBuku *node, *curr;
-// dataBUKU = fopen("FileBuku.txt", "r");
-// while (!feof(dataBUKU)){
-// node = (DataBuku*)malloc(sizeof(DataBuku));
-// node->next = NULL;
-// fscanf(dataBUKU, "%[^#]#%[^#]#%[^#]#%[^#]#%[^\n]\n", node->judulBuku, node->penulis, node->tahunTerbit, node->ISBN, node->jenisBuku);
-// curr = head;
-// if(head == NULL){
-//         head = node;
-//     }
-//     else{
-//         while (curr->next != NULL){
-//             curr = curr->next;
-//         }
-//         curr->next = node;
-//         node->prev = curr;
-//     }
-// }
-// fclose(dataBUKU);
+FILE *dataBUKU;
+DataBuku *node, *curr;
+dataBUKU = fopen("FileBuku.txt", "r");
+while (!feof(dataBUKU)){
+node = (DataBuku*)malloc(sizeof(DataBuku));
+node->next = NULL;
+fscanf(dataBUKU, "%[^#]#%[^#]#%[^#]#%[^#]#%[^\n]\n", node->judulBuku, node->penulis, node->tahunTerbit, node->ISBN, node->jenisBuku);
+curr = head;
+if(head == NULL){
+        head = node;
+    }
+    else{
+        while (curr->next != NULL){
+            curr = curr->next;
+        }
+        curr->next = node;
+        node->prev = curr;
+    }
+}
+fclose(dataBUKU);
 
 defaultMember();
 
@@ -383,7 +383,7 @@ while (1)
 {
    switch (menu()){
         case '1':
-            // displayDataPeminjamanBuku();
+            displayDataPeminjamanBuku();
             break;
         case '2':
             break;
